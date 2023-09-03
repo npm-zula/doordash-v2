2c212e0e4653b1b4d3fb6c2694963f5b95acdf97
aashvr <aashir.intern@devsinc.com>
2023-09-03 11:21:01 +0500

    email based password reset added

--- doordash/doordash/settings.py
+++ doordash/doordash/settings.py
@@ -139,3 +139,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'user.CustomUser'
 
 SESSION_ENGINE = 'doordash.session_backend'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'rajput.azula@gmail.com'
+EMAIL_HOST_PASSWORD = 'lobo uaob xvjl cjsg'
--- doordash/doordash/urls.py
+++ doordash/doordash/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from user import views as v
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -10,4 +11,13 @@ urlpatterns = [
     path('users/', include('user.urls')),
     path('restaurant/', include('restaurant.urls', namespace="restaurant")),
     path('order/', include('order.urls', namespace="order")),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
